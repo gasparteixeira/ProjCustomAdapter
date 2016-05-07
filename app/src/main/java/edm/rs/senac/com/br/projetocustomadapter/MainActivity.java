94ebbb8feb673dec1f0f0aba822bcab368e2f9e6
gasparteixeira <gaspar.teixeira@gmail.com>
2016-05-07 11:54:49 -0300

    adicionado o modelo de contatos e criando e instanciando a list view

--- app/src/main/java/edm/rs/senac/com/br/projetocustomadapter/MainActivity.java
+++ app/src/main/java/edm/rs/senac/com/br/projetocustomadapter/MainActivity.java
@@ -2,12 +2,28 @@ package edm.rs.senac.com.br.projetocustomadapter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private List<Contato> listatContatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listatContatos = new ArrayList<>();
+        listatContatos.add(new Contato("Fulano", "(51) 9999-9999"));
+        listatContatos.add(new Contato("Ciclano", "(51) 9999-9999"));
+        listatContatos.add(new Contato("Beltrano", "(51) 9999-9999"));
+
+        updateList();
+    }
+
+    private void updateList() {
+        ListView listat = (ListView)findViewById(R.id.listView);
     }
 }
